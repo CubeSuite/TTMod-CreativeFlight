2f382e3dbf32025633175b3d9f034f894578806c
CubeSuite <cubesuite@outlook.com>
2024-04-07 18:00:36 +0100

    Add project files.

---
+++ CreativeFlight/CreativeFlightPlugin.cs
@@ -0,0 +1,46 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using CreativeFlight.Patches;
+using HarmonyLib;
+using Rewired;
+using UnityEngine;
+
+namespace CreativeFlight
+{
+    [BepInPlugin(MyGUID, PluginName, VersionString)]
+    public class CreativeFlightPlugin : BaseUnityPlugin
+    {
+        private const string MyGUID = "com.equinox.CreativeFlight";
+        private const string PluginName = "CreativeFlight";
+        private const string VersionString = "0.2.0";
+
+        public static string VerticalSpeedKey = "Vertical Speed";
+        public static ConfigEntry<float> VerticalSpeed;
+
+        public static string AscendShortcutKey = "Ascend Shortcut";
+        public static ConfigEntry<KeyboardShortcut> AscendShortcut;
+
+        public static string DescendShortcutKey = "Descend Shortcut";
+        public static ConfigEntry<KeyboardShortcut> DescendShortcut;
+
+        private static readonly Harmony Harmony = new Harmony(MyGUID);
+        public static ManualLogSource Log = new ManualLogSource(PluginName);
+
+        public static bool isEnabled = true;
+
+        private void Awake() {
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loading...");
+            Harmony.PatchAll();
+
+            VerticalSpeed = Config.Bind("General", VerticalSpeedKey, 0.2f, new ConfigDescription("Speed of ascending and descending", new AcceptableValueRange<float>(-0f, 1.0f)));
+            AscendShortcut = Config.Bind("General", AscendShortcutKey, new KeyboardShortcut(KeyCode.Space), new ConfigDescription("The key to press to ascend"));
+            DescendShortcut = Config.Bind("General", DescendShortcutKey, new KeyboardShortcut(KeyCode.C), new ConfigDescription("The key to press to descend"));
+
+            Harmony.CreateAndPatchAll(typeof(PlayerFirstPersonControllerPatch));
+
+            Logger.LogInfo($"PluginName: {PluginName}, VersionString: {VersionString} is loaded.");
+            Log = Logger;
+        }
+    }
+}
---
+++ CreativeFlight/Patches/PlayerFirstPersonControllerPatch.cs
@@ -0,0 +1,84 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Rewired;
+
+namespace CreativeFlight.Patches
+{
+    internal class PlayerFirstPersonControllerPatch
+    {
+        public static DateTime lastDescendPress = DateTime.Now;
+
+        [HarmonyPatch(typeof(PlayerFirstPersonController), "UpdateHoverPackStatus")]
+        [HarmonyPrefix]
+        public static void AdjustStiltsHeight(PlayerFirstPersonController __instance) {
+            if (!CreativeFlightPlugin.isEnabled) return;
+            Type playerType = typeof(PlayerFirstPersonController);
+            PropertyInfo hoverPackActiveProperty = playerType.GetProperty("hoverPackActive", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Stilts hoverPack = Player.instance.equipment.hoverPack;
+            Type hoverPackType = hoverPack.GetType();
+            FieldInfo stiltsHeightField = hoverPackType.GetField("_stiltHeight", BindingFlags.Instance | BindingFlags.NonPublic);
+            FieldInfo stiltThresholdCloseField = hoverPackType.GetField("_stiltThresholdClose", BindingFlags.Instance | BindingFlags.NonPublic);
+            stiltThresholdCloseField.SetValue(hoverPack, 0);
+
+            if (hoverPackActiveProperty != null && (bool)hoverPackActiveProperty.GetValue(__instance, null)) {
+                float speed = CreativeFlightPlugin.VerticalSpeed.Value;
+
+                if (CreativeFlightPlugin.AscendShortcut.Value.IsPressed()) {
+                    Vector3 position = __instance.transform.position;
+                    position.y += speed;
+                    __instance.transform.position = position;
+
+                    if (stiltsHeightField != null) {
+                        float newStiltsHeightValue = (float)stiltsHeightField.GetValue(hoverPack) + speed;
+                        stiltsHeightField.SetValue(hoverPack, newStiltsHeightValue);
+                    }
+                    else {
+                        Debug.Log("_stiltsHeight field not found");
+                    }
+                }
+                else if (CreativeFlightPlugin.DescendShortcut.Value.IsPressed()) {
+                    Vector3 position = __instance.transform.position;
+                    position.y -= speed;
+                    __instance.transform.position = position;
+
+                    if (stiltsHeightField != null) {
+                        float newStiltsHeightValue = (float)stiltsHeightField.GetValue(hoverPack) - speed;
+                        if (newStiltsHeightValue < 3) newStiltsHeightValue = 3;
+                        stiltsHeightField.SetValue(hoverPack, newStiltsHeightValue);
+                    }
+                    else {
+                        Debug.Log("_stiltsHeight field not found");
+                    }
+                }
+                
+                if (CreativeFlightPlugin.DescendShortcut.Value.IsDown()) {
+                    Debug.Log("Descend Pressed");
+                    double timeBetweenPresses = (DateTime.Now - lastDescendPress).TotalMilliseconds;
+                    Debug.Log($"Time between presses: {timeBetweenPresses}");
+                    if (timeBetweenPresses < 200) {
+                        hoverPack.DeactivateStilts();
+                        stiltsHeightField.SetValue(hoverPack, 3);
+                    }
+
+                    lastDescendPress = DateTime.Now;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerFirstPersonController), "DeactivateHoverPack")]
+        [HarmonyPrefix]
+        public static bool BlockDeactivateHoverpack() {
+            if (!CreativeFlightPlugin.isEnabled) return true;
+            return false;
+        }
+    }
+}
